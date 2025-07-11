f8e5b3e56742fbe44eca12ea4e375568f0ef92a3
mid <mid@k302.kcsk.ac.jp>
2025-07-11 10:27:50 +0900

    testadd

--- src/cook/Main.java
+++ src/cook/Main.java
@@ -3,6 +3,6 @@ package cook;
 public class Main {
 	public static void main(String[] args) {
 	       new MainFrame(); //新しいメインフレームを作成()
-	       System.out.println("test");
+	       System.out.println("test時間10:28");
 	    }
 }
